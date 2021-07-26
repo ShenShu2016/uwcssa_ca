444a698cd0537b15375484cfef36beb25c4ea509
caozeyou <caozeyou@gmail.com>
2021-07-26 17:58:11 -0400

    加了点备注

--- backend/manage.py
+++ backend/manage.py
@@ -16,7 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+# commit 实验
 
 if __name__ == '__main__':
     main()
