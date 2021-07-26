e15234228b9ba858aede5098b640bd060677b0ef
Shen <shushen2013@gmail.com>
2021-07-25 22:23:01 -0400

    delete from setting
    delete from setting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from SECRET import SECRET_KEY, ALLOWED_HOSTS, DATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,17 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1o$08h&&#!z#0%=%h2f84tf-#hjo%gudu^abv_veh4ab5g&)4z'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '198.211.99.20', 
-    'localhost', 
-    '127.0.0.1',
-    "0.0.0.0",
-]
+
 
 
 # Application definition
@@ -81,16 +76,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+
 
 
 # Password validation
