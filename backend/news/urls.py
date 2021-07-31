4492ab782cf88a91edab7997d017d38828d0fb59
Shen <shushen2013@gmail.com>
2021-07-31 19:04:08 -0400

    move to api.urls

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,14 +1,7 @@
-from django.urls import include, path
 from rest_framework import routers
 from news import views
 
 
 router = routers.DefaultRouter()
 router.register(r'news/topic', views.TopicViewSet)
-router.register(r'news/news', viewset=views.NewsViewSet)
-
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-    path('', views.News, name='News')
-]
+router.register(r'news/article', views.ArticleViewSet)
