8b1fbc9539098ca5da0ada620d923ba9399f7980
Shen <shushen2013@gmail.com>
2021-08-01 16:05:48 -0400

    fix: connect news table to CustomUser table

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
 
 
@@ -8,11 +8,11 @@ from django.db import models
 # reference: https://blog.csdn.net/qq_39208536/article/details/103761445
 
 class Topic(models.Model):
-    topic = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255, unique=True)
     # auto_now_add，默认值为false，设置为true时，会在model对象第一次被创建时，将字段的值设置为创建时的时间，以后修改对象时，字段的值不会再更新。
     last_updated = models.DateTimeField(auto_now_add=True)
     # will use User model later
-    starter = models.CharField(max_length=64, null=False)
+    starter = models.ForeignKey(CustomUser, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.topic
@@ -30,8 +30,8 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     # will use User model later
-    created_by = models.CharField(max_length=64, null=False)
-    updated_by = models.CharField(max_length=64, null=False)
+    created_by = models.ForeignKey(CustomUser, null=False, on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE, related_name='+')
 
     def __str__(self):
         return self.subject
