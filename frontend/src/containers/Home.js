cf1d20820534775d9e4c23db505f5e1ceb1c76b6
Shen <shushen2013@gmail.com>
2021-08-21 17:35:44 -0400

    feat: homepage ticker

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../App.css';
-import Card from '../components/card/Card'
-
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import Card from "../components/card/Card";
+import HomeTickers from "../components/home_components/HomeTickers";
 const Home = () => (
-    <>
-      <h1 className="home">Home</h1>
+  <Fragment>
+    {/* <h1 className="home">Home</h1>
       <Card 
       apiAddress='http://127.0.0.1:8000/news/article_list/'
       index='0'
@@ -13,8 +13,9 @@ const Home = () => (
       <Card 
       apiAddress='http://127.0.0.1:8000/news/article_list/'
       index='1'
-      />
-    </>
+      /> */}
+    <HomeTickers />
+  </Fragment>
 );
 
 export default Home;
