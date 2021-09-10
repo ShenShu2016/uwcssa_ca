7b5bbfad8916c5bd9e77252084fc73b41965b135
Shen <shushen2013@gmail.com>
2021-09-10 00:47:15 -0400

    feat: count total user
    using Axios and Redux

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { productReducer, selectedProductReducer } from "./productReducer";
 import { authReducer } from "./authReducer";
-
+import { userReducer } from './userReducer'
 export default combineReducers({
   allProducts: productReducer,
   product: selectedProductReducer,
   userAuth: authReducer,
+  allUsers: userReducer,
 });
