c1b7b5d99e2e94a6e6d239afa95c461897d6cf28
IreneDai0822 <shiwen.irenedai@gmail.com>
2021-10-05 18:56:05 -0400

    fix: delete import never use

--- frontend/src/containers/FourmHome.js
+++ frontend/src/containers/FourmHome.js
@@ -1,38 +1,35 @@
+import ForumLeftSide from "../components/Forum/ForumLeftSide";
+import ForumRightSide from "../components/Forum/ForumRightSide";
 import Grid from "@material-ui/core/Grid";
-import { Hidden } from '@material-ui/core';
-import React from 'react'
+import { Hidden } from "@material-ui/core";
+import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import Divider from '@material-ui/core/Divider';
-import ForumRightSide from '../components/Forum/ForumRightSide';
-import ForumLeftSide from '../components/Forum/ForumLeftSide';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "#fff",
+    display: "flex",
+    flexDirection: "row",
+  },
+}));
 
-    root: {
-        background:"#fff",
-        display:"flex",
-        flexDirection:"row",
-        
-    },
-
-    })
-)
 const FourmHome = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={0}>
-                <Grid item xs={12} sm={11} md={10} lg={10} xl={10}>
-                    <ForumLeftSide/>
-                </Grid>
-                <Hidden xsDown>
-                <Grid item sm={1} md={2}>
-                    <ForumRightSide/>
-                </Grid>
-                </Hidden>
-            </Grid>
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={11} md={10} lg={10} xl={10}>
+          <ForumLeftSide />
+        </Grid>
+        <Hidden xsDown>
+          <Grid item sm={1} md={2}>
+            <ForumRightSide />
+          </Grid>
+        </Hidden>
+      </Grid>
+    </div>
+  );
 };
 
 export default FourmHome;
