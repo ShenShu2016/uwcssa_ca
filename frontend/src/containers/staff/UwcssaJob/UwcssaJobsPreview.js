5252ffafab96b41b2670c5cd96a4f60c281cb3c2
mike <mikezerone@hotmail.com>
2021-10-14 15:28:45 -0400

    feat: modified uwcssaJobs preview page

--- frontend/src/containers/staff/UwcssaJob/UwcssaJobsPreview.js
+++ frontend/src/containers/staff/UwcssaJob/UwcssaJobsPreview.js
@@ -4,7 +4,7 @@ import {
   setUwcssaJobs,
 } from "../../../redux/actions/uwcssaJobActions";
 import { useDispatch, useSelector } from "react-redux";
-
+import { DataGrid } from "@mui/x-data-grid";
 import { Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
@@ -14,6 +14,49 @@ const useStyles = makeStyles({
     margin: "auto",
   },
 });
+
+const columns = [
+  { field: "id", headerName: "ID", width: 150 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 200,
+    editable: false,
+  },
+  {
+    field: "department",
+    headerName: "Department",
+    width: 110,
+    editable: false,
+  },
+  {
+    field: "like",
+    headerName: "Like",
+    width: 100,
+    type: "number",
+    editable: false,
+  },
+  {
+    field: "unlike",
+    headerName: "Unlike",
+    width: 100,
+    type: "number",
+    editable: false,
+  },
+  {
+    field: "active",
+    headerName: "Active",
+    width: 100,
+    editable: false,
+  },
+  {
+    field: "applied",
+    headerName: "Applied",
+    width: 100,
+    editable: false,
+  },
+];
+
 export default function UwcssaJobsPreview() {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -29,9 +72,40 @@ export default function UwcssaJobsPreview() {
   );
   console.log("departments, uwcssaJobs", departments, uwcssaJobs);
 
+  const rows = uwcssaJobs.map((uwcssaJob) => {
+    const {
+      id,
+      title,
+      department,
+      like,
+      unlike,
+      active,
+      uwcssaJobResumes,
+    } = uwcssaJob;
+    return {
+      id: id,
+      title: title,
+      department: department.name,
+      like: like.filter(e=>e!=="").length,
+      unlike: unlike.filter(e=>e!=="").length,
+      active: active,
+      applied: uwcssaJobResumes.items.length,
+    };
+  });
+
   return (
     <div className={classes.root}>
-      <Typography variant="h1">Uwcssa Jobs Preview</Typography>
+      <Typography variant="h4">Uwcssa Jobs Preview</Typography>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </div>
     </div>
   );
 }
