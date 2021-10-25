d2f29ed58e78f927ba6380715319f5347cf699d8
Shen <shushen2013@gmail.com>
2021-10-25 01:36:08 -0400

    feat: Customize Avatar with real pic

--- frontend/src/components/CustomMUI/CustomAvatar.js
+++ frontend/src/components/CustomMUI/CustomAvatar.js
@@ -1,6 +1,10 @@
+import React, { useEffect, useState } from "react";
+
+import API from "@aws-amplify/api";
 import { Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
-import React from "react";
+import Storage from "@aws-amplify/storage";
+import { getUser } from "../../graphql/queries";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
@@ -37,11 +41,53 @@ function stringAvatar(name) {
 
 export default function CustomAvatar({ username, variant, sx, link }) {
   const classes = useStyles();
+  const [userData, setUserData] = useState("");
+  const [avatarURL, setAvatarURL] = useState(null);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await API.graphql({
+          query: getUser,
+          variables: { id: username },
+          authMode: "AWS_IAM",
+        });
+        setUserData(response.data.getUser);
+      } catch (error) {
+        console.log("error on selecting User", error);
+      }
+    };
+    if (username) {
+      getUserData();
+    }
+  }, [username]);
+  console.log(userData);
+
+  useEffect(() => {
+    const getAvatarImage = async () => {
+      try {
+        const imageAccessURL = await Storage.get(userData.avatarImgPath, {
+          level: "public",
+          expires: 120,
+          download: false,
+        });
+        setAvatarURL(imageAccessURL);
+      } catch (error) {
+        console.error("error accessing the Image from s3", error);
+        setAvatarURL(null);
+      }
+    };
+    if (userData.avatarImgPath) {
+      getAvatarImage();
+    }
+  }, [userData]);
+
   return (
     <div>
       <Avatar
         component={link === true ? Link : ""}
         to={link === true ? `/account/profile/${username}` : ""}
+        src={avatarURL}
         variant={variant}
         className={classes.avatar}
         style={sx}
