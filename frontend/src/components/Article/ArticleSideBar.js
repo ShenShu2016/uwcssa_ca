c678cee5d3859c80655c09e26dcc4c920d7458c5
Shen <shushen2013@gmail.com>
2021-10-31 18:38:19 -0400

    feat: init google adSense test

--- frontend/src/components/Article/ArticleSideBar.js
+++ frontend/src/components/Article/ArticleSideBar.js
@@ -1,7 +1,7 @@
+import ArticleSideBarAD from "../AdSense/ArticleSideBarAD";
 import { Paper } from "@mui/material";
 import React from "react";
 import { makeStyles } from "@mui/styles";
-
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: "1rem",
@@ -10,6 +10,7 @@ const useStyles = makeStyles((theme) => ({
     position: "sticky",
     width: "310px",
     top: "8rem",
+    textAlign: "center",
     [theme.breakpoints.down("lg")]: {
       position: "relative",
       top: "0",
@@ -27,7 +28,11 @@ function ArticleSideBar() {
   const classes = useStyles();
   return (
     <Paper variant="outlined" className={classes.root}>
-      我里面要放点广告拉我里面要放点广告拉我里面要放点广告拉我里面要放点广告拉我里面要放点广告拉我里面要放点广告拉
+      <img
+        src="https://media-exp1.licdn.com/dms/image/C5603AQHwt3NgA8rYHw/profile-displayphoto-shrink_200_200/0/1616353723146?e=1640822400&v=beta&t=wzrF9eUlq7YnsTg-1cpH4LrYXm2oCCOHHHp0ac1hmgM"
+        alt="作者"
+      />
+      <ArticleSideBarAD />
     </Paper>
   );
 }
