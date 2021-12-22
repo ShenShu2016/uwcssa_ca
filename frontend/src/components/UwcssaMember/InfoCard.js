8c4c015da59755fdb979c94926fd5db194abe81f
Shikai <shikaijin7@gmail.com>
2021-12-21 21:18:24 -0500

    fix: change avatar size

--- frontend/src/components/UwcssaMember/InfoCard.js
+++ frontend/src/components/UwcssaMember/InfoCard.js
@@ -60,8 +60,8 @@ const useStyles = makeStyles((palette) => ({
     },
   },
   avatar: {
-    width: 90,
-    height: 90,
+    width: 120,
+    height: 120,
     margin: "auto",
     borderRadius: 12,
     size: 48,
