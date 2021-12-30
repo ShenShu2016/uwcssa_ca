07def847410f26a76b4a47cabfa1173a3c0ff341
Shikai <shikaijin7@gmail.com>
2021-12-30 00:24:40 -0500

    feat: add setTimeout to no permission page

--- frontend/src/containers/NoPermission.js
+++ frontend/src/containers/NoPermission.js
@@ -3,7 +3,7 @@ import { Button, Typography } from "@mui/material";
 import React, { Fragment } from "react";
 
 import Footer from "./Footer";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -12,6 +12,7 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     // margin: "4rem auto",
     maxWidth: "100%",
+    height: "75%",
     paddingInline: "2rem",
     // color: "#0D1F48",
   },
@@ -19,6 +20,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function NoPermission() {
   const classes = useStyles();
+  const history = useHistory();
+
+  setTimeout(function () {
+    history.push("/");
+  }, 10000);
 
   return (
     <Fragment>
@@ -27,13 +33,14 @@ export default function NoPermission() {
           <img
             src="https://textcaseconvert.com/blog/wp-content/uploads/2020/08/ohno.gif"
             alt="你没有权限哦"
-            width="30%"
+            width="50%"
           />
         </Box>
         <Box sx={{ my: "2rem" }}>
-          <Typography variant="h5">
+          <Typography variant="h5" gutterBottom>
             权限不足 获取权限请加入UWCSSA学生会
           </Typography>
+          <Typography variant="h6">10秒后将自动转到主页...</Typography>
         </Box>
         <Box sx={{ my: "2rem" }}>
           <ButtonGroup variant="text">
