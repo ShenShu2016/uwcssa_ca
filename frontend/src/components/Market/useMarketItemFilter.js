321d0aa08521ee8532727c21e9de48eeaf012ab8
Yichao Ma <61528973+Bubbrex@users.noreply.github.com>
2022-01-03 04:16:31 +0000

    fix: fix market filter bugs

--- frontend/src/components/Market/useMarketItemFilter.js
+++ frontend/src/components/Market/useMarketItemFilter.js
@@ -8,44 +8,123 @@ import { useEffect, useState } from "react";
 import { fetchMarketItems } from "../../redux/slice/marketSlice";
 import { useDispatch } from "react-redux";
 
-export default function useMarketItemFilter(filterList) {
+export default function useMarketItemFilter(props) {
+  const {
+    type,
+    category,
+    condition,
+    min,
+    max,
+    minYear,
+    maxYear,
+    make,
+    model,
+    vehicleType,
+    marketRentalSaleRent,
+    propertyType,
+    airConditioningType,
+    heatingType,
+  } = props;
+
   const dispatch = useDispatch();
   const [isFiltering, setIsFiltering] = useState(false);
   const query =
-    filterList.type === "item"
+    type === "Item"
       ? marketItemSortBySortKeyItem
-      : filterList.type === "vehicle"
+      : type === "Vehicle"
       ? marketItemSortBySortKeyVehicle
-      : filterList.type === "rental"
+      : type === "Rental"
       ? marketItemSortBySortKeyRental
       : null;
+
   useEffect(() => {
     let filter = {};
-
-    if (filterList.category !== "") {
-      filter["marketItemCategory"] = { eq: filterList.category };
-    }
-    if (filterList.min !== "") {
-      if (filterList.max !== "") {
-        filter["price"] = { between: [filterList.min, filterList.max] };
+    // general filter;
+    if (min !== "") {
+      if (max !== "") {
+        filter["price"] = { between: [min, max] };
       } else {
-        filter["price"] = { ge: filterList.min };
+        filter["price"] = { ge: min };
       }
     } else {
-      if (filterList.max !== "") {
-        filter["price"] = { le: filterList.max };
+      if (max !== "") {
+        filter["price"] = { le: max };
       }
+    }
+    // Item filter;
+    if (category !== undefined && category !== "") {
+      filter["marketItemCategory"] = { eq: category };
+    }
+    if (condition !== undefined && condition !== "") {
+      filter["marketItemCondition"] = { eq: condition };
+    }
+    // vehicle filter;
+    if (vehicleType !== undefined && vehicleType !== "") {
+      filter["vehicleType"] = { eq: vehicleType };
+    }
+    if (minYear !== undefined && minYear !== "") {
+      if (maxYear !== "") {
+        filter["year"] = { between: [minYear, maxYear] };
+      } else {
+        filter["year"] = { ge: minYear };
+      }
+    } else {
+      if (maxYear !== undefined && maxYear !== "") {
+        filter["year"] = { le: maxYear };
+      }
+    }
+    if (make !== undefined && make !== "") {
+      filter["make"] = { eq: make };
+    }
+    if (model !== undefined && model !== "") {
+      filter["model"] = { eq: model };
+    }
+    // Rental filter;
+    if (marketRentalSaleRent !== undefined && marketRentalSaleRent !== "") {
+      filter["marketRentalSaleRent"] = { eq: marketRentalSaleRent };
+    }
+    if (propertyType !== undefined && propertyType !== "") {
+      filter["propertyType"] = { eq: propertyType };
+    }
+    if (airConditioningType !== undefined && airConditioningType !== "") {
+      filter["airConditioningType"] = { eq: airConditioningType };
+    }
+    if (heatingType !== undefined && heatingType !== "") {
+      filter["heatingType"] = { eq: heatingType };
     }
     if (query !== null) {
       if (Object.keys(filter).length === 0) {
         setIsFiltering(false);
-        dispatch(fetchMarketItems({ query: query }));
+        dispatch(
+          fetchMarketItems({
+            query: query,
+            filter: { marketType: { eq: type } },
+          })
+        );
       } else {
         setIsFiltering(true);
+        filter["marketType"] = { eq: type };
         dispatch(fetchMarketItems({ query: query, filter }));
       }
     }
-  }, [dispatch, filterList, query]);
+  }, [
+    dispatch,
+    query,
+    type,
+    category,
+    condition,
+    min,
+    max,
+    minYear,
+    maxYear,
+    make,
+    model,
+    vehicleType,
+    marketRentalSaleRent,
+    propertyType,
+    airConditioningType,
+    heatingType,
+  ]);
 
   return isFiltering;
 }
