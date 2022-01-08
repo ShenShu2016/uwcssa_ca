6fe03592b3d46b7da83586d4ae8e264c0be0cdf4
Shikai <shikaijin7@gmail.com>
2022-01-08 17:08:05 -0500

    fix: edit some event UI

--- frontend/src/components/Event/EventTable.js
+++ frontend/src/components/Event/EventTable.js
@@ -31,6 +31,12 @@ import { makeStyles } from "@mui/styles";
 import EditIcon from "@mui/icons-material/Edit";
 import moment from "moment";
 import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import { useTheme } from "@mui/material/styles";
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -132,6 +138,75 @@ EnhancedTableToolbar.propTypes = {
   numSelected: PropTypes.number.isRequired,
 };
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 export default function SimpleTable() {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -186,6 +261,7 @@ export default function SimpleTable() {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
+
   const downloadExcel = (dataset, title) => {
     const newData = dataset.map((row) => {
       delete row.tableData;
@@ -220,6 +296,7 @@ export default function SimpleTable() {
         </Box>
         <TableContainer component={Paper}>
           <Table
+            stickyHeader
             sx={{ minWidth: 500 }}
             // rowsPerPageOptions={[5]}
           >
@@ -287,6 +364,7 @@ export default function SimpleTable() {
                                 <TableCell>电话</TableCell>
                                 <TableCell>人数</TableCell>
                                 <TableCell>地址</TableCell>
+                                <TableCell>状态</TableCell>
                                 <TableCell>备注</TableCell>
                               </TableRow>
                             </TableHead>
@@ -315,6 +393,9 @@ export default function SimpleTable() {
                                     <TableCell>
                                       {eventParticipant.address &&
                                         eventParticipant.address.description}
+                                    </TableCell>
+                                    <TableCell>
+                                      {eventParticipant.eventParticipantStatus}
                                     </TableCell>
                                     <TableCell>
                                       {eventParticipant.message}
@@ -360,7 +441,7 @@ export default function SimpleTable() {
                               placement="right"
                               arrow
                             >
-                              <ErrorOutlineRoundedIcon />
+                              <ErrorOutlineRoundedIcon color="error" />
                             </Tooltip>
                           ) : (
                             <div>
@@ -372,7 +453,7 @@ export default function SimpleTable() {
                                   placement="right"
                                   arrow
                                 >
-                                  <ErrorOutlineRoundedIcon />
+                                  <WarningAmberIcon color="warning" />
                                 </Tooltip>
                               ) : null}
                             </div>
@@ -391,12 +472,13 @@ export default function SimpleTable() {
             <TableFooter>
               <TableRow>
                 <TablePagination
-                  rowsPerPageOptions={[5, 10, 25]}
+                  rowsPerPageOptions={[5, 10, 25, { label: "所有", value: -1 }]}
                   count={rows.length}
                   rowsPerPage={rowsPerPage}
                   page={page}
                   onPageChange={handleChangePage}
                   onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
                 />
               </TableRow>
             </TableFooter>
