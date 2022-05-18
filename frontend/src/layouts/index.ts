0214726eecc515bae32e3759eff68b3cf0005702
Shen <shushen2013@gmail.com>
2022-05-18 19:29:29 -0400

    feat!: remove layout fixed and fluid, because current we don't use it

--- frontend/src/layouts/index.ts
+++ frontend/src/layouts/index.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: Shen Shu
+ * @Date: 2022-05-17 21:41:42
+ * @LastEditors: Shen Shu
+ * @LastEditTime: 2022-05-18 19:26:56
+ * @FilePath: \uwcssa_ca\frontend\src\layouts\index.ts
+ * @Description:
+ *
+ */
 export { default as Main } from './Main';
-export { default as Fluid } from './Fluid';
-export { default as Fixed } from './Fixed';
+// export { default as Fluid } from './Fluid';
+// export { default as Fixed } from './Fixed';
