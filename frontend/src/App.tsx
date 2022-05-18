077522c486184f3db7a76de898940c3cf67fbab7
Shen <shushen2013@gmail.com>
2022-05-18 19:02:23 -0400

    feat: add loaduser when open app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,33 @@
  * @Author: Shen Shu
  * @Date: 2022-05-17 14:08:10
  * @LastEditors: Shen Shu
- * @LastEditTime: 2022-05-17 14:25:51
+ * @LastEditTime: 2022-05-18 17:53:57
  * @FilePath: \uwcssa_ca\frontend\src\App.tsx
  * @Description:
  *
  */
+
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-image-lightbox/style.css';
 import 'aos/dist/aos.css';
 
+import React, { useEffect } from 'react';
+
 import { BrowserRouter } from 'react-router-dom';
 import Page from './components/Page';
-import React from 'react';
 import Routes from './Routes';
+import { loadUser } from 'redux/auth/authSlice';
+import { useAppDispatch } from 'redux/hooks';
 
 const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
   return (
     <Page>
       <BrowserRouter>
