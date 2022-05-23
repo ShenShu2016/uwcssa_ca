6544a8e4c69f55bcc92dd1950815b827b19badfa
Shen <shushen2013@gmail.com>
2022-05-23 11:56:07 -0400

    feat: update ContactUsHandler with permission and sending email teamplate

--- frontend/amplify/backend/function/ContactUsHandler/src/index.py
+++ frontend/amplify/backend/function/ContactUsHandler/src/index.py
@@ -2,20 +2,96 @@
 Author: Shen Shu
 Date: 2022-05-20 16:29:06
 LastEditors: Shen Shu
-LastEditTime: 2022-05-20 23:52:22
+LastEditTime: 2022-05-23 11:54:54
 FilePath: /uwcssa_ca/frontend/amplify/backend/function/ContactUsHandler/src/index.py
 Description: 
 
 '''
 import json
+
 import boto3
+
 # https://www.learnaws.org/2020/12/18/aws-ses-boto3-guide/#how-to-send-an-email-using-ses
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
+    print('received event:')
+    print(event)
+    ses_client = boto3.client("ses", region_name="us-east-1")
+    CHARSET = "UTF-8"
+
+    HTML_EMAIL_CONTENT1 =f"""
+        <html>
+            <head></head>
+            <h1 style='text-align:center'>这是您提交的信息</h1>
+            <p>姓名： {event['Records'][0]['dynamodb']['NewImage']['fullName']['S']}</p>
+            <p>电话： {event['Records'][0]['dynamodb']['NewImage']['phone']['S']}</p>
+            <p>email: {event['Records'][0]['dynamodb']['NewImage']['email']['S']}</p>
+            <p>Message: {event['Records'][0]['dynamodb']['NewImage']['message']['S']}</p>
+            </body>
+        </html>
+    """
+    try:
+        e = 'email发送成功'
+        ses_client.send_email(
+            Destination={
+                "ToAddresses": [
+                    "shushen2013@gmail.com",
+                ],
+            },
+            Message={
+                "Body": {
+                    "Html": {
+                        "Charset": CHARSET,
+                        "Data": HTML_EMAIL_CONTENT1,
+                    }
+                },
+                "Subject": {
+                    "Charset": CHARSET,
+                    "Data": "UWCSSA 感谢您的提问，我们会尽快解决问题",
+                },
+            },
+            Source="Shen Shu <admin@uwcssa.ca>",
+        )
+    except Exception as e:
+        print(e)
+
+
+    HTML_EMAIL_CONTENT2 =f"""
+        <html>
+            <head></head>
+            <h1 style='text-align:center'>有人联系我们啦</h1>
+            <p>姓名： {event['Records'][0]['dynamodb']['NewImage']['fullName']['S']}</p>
+            <p>电话： {event['Records'][0]['dynamodb']['NewImage']['phone']['S']}</p>
+            <p>email {event['Records'][0]['dynamodb']['NewImage']['email']['S']}</p>
+            <p>Message {event['Records'][0]['dynamodb']['NewImage']['message']['S']}</p>
+            <p>回执是否成功 {e}</p>
+            </body>
+        </html>
+    """
+
+    ses_client.send_email(
+        Destination={
+            "ToAddresses": [
+                "shushen2013@gmail.com",
+            ],
+        },
+        Message={
+            "Body": {
+                "Html": {
+                    "Charset": CHARSET,
+                    "Data": HTML_EMAIL_CONTENT2,
+                }
+            },
+            "Subject": {
+                "Charset": CHARSET,
+                "Data": "UWCSSA.CA 有人联系我们",
+            },
+        },
+        Source="Shen Shu <admin@uwcssa.ca>",
+    )
+
+
+    return {
       'statusCode': 200,
       'headers': {
           'Access-Control-Allow-Headers': '*',
---
+++ frontend/amplify/backend/function/ContactUsHandler/src/test.py
@@ -0,0 +1,17 @@
+'''
+Author: Shen Shu
+Date: 2022-05-23 00:21:29
+LastEditors: Shen Shu
+LastEditTime: 2022-05-23 00:25:18
+FilePath: /uwcssa_ca/frontend/amplify/backend/function/ContactUsHandler/src/test.py
+Description: 
+
+'''
+
+
+event={'Records': [{'eventID': '7dccd6ebdf6a75f07e82c1ea31e04573', 'eventName': 'INSERT', 'eventVersion': '1.1', 'eventSource': 'aws:dynamodb', 'awsRegion': 'us-east-2', 'dynamodb': {'ApproximateCreationDateTime': 1653279414.0, 'Keys': {'id': {'S': '43bed9bd-067a-415b-9fed-fa00f83a1a62'}}, 'NewImage': {'owner': {'S': '6a0cba8e-aa9f-4991-b6df-5f4a7b73cf61'}, 'createdAt': {'S': '2022-05-23T04:16:53.613Z'}, 'phone': {'S': '6478710781'}, '__typename': {'S': 'ContactUs'}, 'fullName': {'S': 'sdfsd '}, 'id': {'S': '43bed9bd-067a-415b-9fed-fa00f83a1a62'}, 'message': {'S': '4d56s4f56ds4f65s45fa64 65f4 sda6 f'}, 'email': {'S': 'shushengacademy@gmail.com'}, 'updatedAt': {'S': '2022-05-23T04:16:53.613Z'}}, 'SequenceNumber': '14236400000000000560585594', 'SizeBytes': 302, 'StreamViewType': 'NEW_AND_OLD_IMAGES'}, 'eventSourceARN': 'arn:aws:dynamodb:us-east-2:537666141782:table/ContactUs-4r2t6l2uprdgjhmoocpazejjcq-devts/stream/2022-05-20T03:38:37.566'}]}
+#print(event)
+print(event['Records'][0]['dynamodb']['NewImage']['fullName']['S'])
+print(event['Records'][0]['dynamodb']['NewImage']['message']['S'])
+print(event['Records'][0]['dynamodb']['NewImage']['email']['S'])
+print(event['Records'][0]['dynamodb']['NewImage']['phone']['S'])
