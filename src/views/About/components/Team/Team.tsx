d7b186d5f50279ea7407428e9410c43fec1538b1
Shikai <shikaijin7@gmail.com>
2022-06-07 18:20:34 -0400

    feat: add uwcssa members to about us

--- src/views/About/components/Team/Team.tsx
+++ src/views/About/components/Team/Team.tsx
@@ -1,3 +1,14 @@
+import React, { useEffect } from 'react';
+import {
+  fetchUwcssaDepartmentList,
+  selectAllUwcssaDepartments,
+} from 'redux/uwcssaDepartment/uwcssaDepartmentSlice';
+import {
+  fetchUwcssaMemberList,
+  selectAllUwcssaMembers,
+} from 'redux/uwcssaMember/uwcssaMemberSlice';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
+
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -6,63 +17,40 @@ import Grid from '@mui/material/Grid';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import React from 'react';
 import Typography from '@mui/material/Typography';
+import { getAuthState } from 'redux/auth/authSlice';
+import { stringAvatar } from 'components/Avatar/AvatarFunction';
 import { useTheme } from '@mui/material/styles';
-
-const mocks = [
-  {
-    name: 'Clara Bertoletti',
-    title: 'MUI lover',
-    avatar: 'https://assets.maccarianagency.com/avatars/img4.jpg',
-    department: '主席团',
-  },
-  {
-    name: 'sdfsdfsf',
-    title: 'MUI lover',
-    avatar: 'https://assets.maccarianagency.com/avatars/img5.jpg',
-    department: '主席团',
-  },
-  {
-    name: 'sdfsdf',
-    title: 'MUI lover',
-    avatar: 'https://assets.maccarianagency.com/avatars/img4.jpg',
-    department: '主席团',
-  },
-  {
-    name: 'Jhon Anderson',
-    title: 'Senior Frontend Developer',
-    avatar: 'https://assets.maccarianagency.com/avatars/img5.jpg',
-    department: '活动部',
-  },
-  {
-    name: 'Chary Smith',
-    title: 'SEO at Comoti',
-    avatar: 'https://assets.maccarianagency.com/avatars/img6.jpg',
-    department: '外联部',
-  },
-  {
-    name: 'Clara Bertoletti',
-    title: 'MUI lover',
-    avatar: 'https://assets.maccarianagency.com/avatars/img1.jpg',
-    department: '活动部',
-  },
-  {
-    name: 'Jhon Anderson',
-    title: 'Senior Frontend Developer',
-    avatar: 'https://assets.maccarianagency.com/avatars/img2.jpg',
-    department: '技术部',
-  },
-  {
-    name: 'Chary Smith',
-    title: 'SEO at Comoti',
-    avatar: 'https://assets.maccarianagency.com/avatars/img3.jpg',
-    department: '文体部',
-  },
-];
 
 const Team = (): JSX.Element => {
   const theme = useTheme();
+  const isAuth = useAppSelector(getAuthState);
+  const dispatch = useAppDispatch();
+  const departments = useAppSelector(selectAllUwcssaDepartments);
+  const uwcssaMembers = useAppSelector(selectAllUwcssaMembers);
+  const { fetchUwcssaMemberListStatus } = useAppSelector(
+    (state) => state.uwcssaMember,
+  );
+  const { fetchUwcssaDepartmentListStatus } = useAppSelector(
+    (state) => state.uwcssaDepartment,
+  );
+  useEffect(() => {
+    const getUwcssaMembers = async () => {
+      if (isAuth !== null && fetchUwcssaMemberListStatus === 'idle') {
+        await dispatch(fetchUwcssaMemberList({ isAuth }));
+      }
+    };
+    getUwcssaMembers();
+  }, [fetchUwcssaMemberListStatus]);
+
+  useEffect(() => {
+    const getUwcssaDepartments = async () => {
+      if (isAuth !== null && fetchUwcssaDepartmentListStatus === 'idle') {
+        await dispatch(fetchUwcssaDepartmentList({ isAuth }));
+      }
+    };
+    getUwcssaDepartments();
+  }, [fetchUwcssaDepartmentListStatus]);
 
   return (
     <Box>
@@ -78,48 +66,75 @@ const Team = (): JSX.Element => {
         >
           学生会
         </Typography>
-        <Typography variant="h6" align={'center'} color={'text.secondary'}>
-          dfgdfgdg
-        </Typography>
       </Box>
-      <Grid container spacing={2}>
-        {mocks.map((item, i) => {
+      <>
+        {departments.map((department, i) => {
           return (
-            <Grid item xs={12} md={4} key={i}>
-              <Box
-                width={1}
-                height={1}
-                component={Card}
-                boxShadow={0}
-                variant={'outlined'}
-                bgcolor={'alternate.main'}
-              >
-                <CardContent sx={{ padding: 3 }}>
-                  <ListItem component="div" disableGutters sx={{ padding: 0 }}>
-                    <ListItemAvatar sx={{ marginRight: 3 }}>
-                      <Avatar
-                        src={item.avatar}
-                        variant={'rounded'}
-                        sx={{ width: 100, height: 100, borderRadius: 2 }}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText
-                      sx={{ margin: 0 }}
-                      primary={item.name}
-                      secondary={item.title}
-                      primaryTypographyProps={{
-                        variant: 'h6',
-                        fontWeight: 700,
-                      }}
-                      secondaryTypographyProps={{ variant: 'subtitle1' }}
-                    />
-                  </ListItem>
-                </CardContent>
+            <Box key={i} marginBottom={4}>
+              <Box marginBottom={4}>
+                <Typography
+                  variant="h6"
+                  align={'center'}
+                  color={'text.secondary'}
+                >
+                  {department.id}
+                </Typography>
               </Box>
-            </Grid>
+
+              <Grid container spacing={2}>
+                {uwcssaMembers
+                  .filter(
+                    (x) => x.uwcssaDepartmentUwcssaMembersId === department.id,
+                  )
+                  .map((item, i) => {
+                    return (
+                      <Grid item xs={12} md={4} key={i}>
+                        <Box
+                          width={1}
+                          height={1}
+                          component={Card}
+                          boxShadow={0}
+                          variant={'outlined'}
+                          bgcolor={'alternate.main'}
+                        >
+                          <CardContent sx={{ padding: 3 }}>
+                            <ListItem
+                              component="div"
+                              disableGutters
+                              sx={{ padding: 0 }}
+                            >
+                              <ListItemAvatar sx={{ marginRight: 3 }}>
+                                <Avatar
+                                  src={item.user.avatarURL?.objectCompressedURL}
+                                  {...stringAvatar(item.user.name, {
+                                    width: 80,
+                                    height: 80,
+                                  })}
+                                />
+                              </ListItemAvatar>
+                              <ListItemText
+                                sx={{ margin: 0 }}
+                                primary={item.name || 'null'}
+                                secondary={item.title || 'null'}
+                                primaryTypographyProps={{
+                                  variant: 'h6',
+                                  fontWeight: 700,
+                                }}
+                                secondaryTypographyProps={{
+                                  variant: 'subtitle1',
+                                }}
+                              />
+                            </ListItem>
+                          </CardContent>
+                        </Box>
+                      </Grid>
+                    );
+                  })}
+              </Grid>
+            </Box>
           );
         })}
-      </Grid>
+      </>
     </Box>
   );
 };
