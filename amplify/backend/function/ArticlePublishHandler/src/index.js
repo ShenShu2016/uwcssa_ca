07ff8a0b020b900970eba3fe263d69817cf777e3
Shen <shushen2013@gmail.com>
2022-06-11 20:03:50 -0400

    fix: 所有lambda 都需要迭代 record

--- amplify/backend/function/ArticlePublishHandler/src/index.js
+++ amplify/backend/function/ArticlePublishHandler/src/index.js
@@ -2,7 +2,7 @@
  * @Author: Shikai Jin
  * @Date: 2022-05-28 22:04:05
  * @LastEditors: Shen Shu
- * @LastEditTime: 2022-06-10 18:23:48
+ * @LastEditTime: 2022-06-11 19:57:40
  * @FilePath: /uwcssa_ca/amplify/backend/function/ArticlePublishHandler/src/index.js
  * @Description:
  *
@@ -19,70 +19,72 @@ var docClient = new AWS.DynamoDB.DocumentClient({ region: process.env.REGION });
 // eslint-disable-next-line no-undef
 exports.handler = async function (event) {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  const record = event['Records'][0];
-  if (record.eventName !== 'INSERT' && record.eventName !== 'MODIFY') {
-    console.log('Event is not INSERT or MODIFY, ignore');
-    return Promise.resolve('not an insert event or modify event');
-  }
-  if (record.dynamodb.NewImage.isPublish.BOOL === false) {
-    console.log('Article is not published, ignore');
-    return Promise.resolve('did not publish');
-  }
-  console.log(record.eventID);
-  console.log(record.eventName);
-  console.log('DynamoDB Record: %j', record.dynamodb);
-  const tableName = process.env.UserProfile_Table;
-  let dbParams = {
-    TableName: tableName,
-    FilterExpression: 'emailSubscription = :emailSubscription',
-    ExpressionAttributeValues: {
-      ':emailSubscription': true,
-    },
-  };
-  let scanResults = [];
-  let items;
-  //没有处理多于50个人的情况
-  do {
-    items = await docClient.scan(dbParams).promise();
-    items.Items.forEach((item) => scanResults.push(item));
-    dbParams.ExclusiveStartKey = items.LastEvaluatedKey;
-  } while (typeof items.LastEvaluatedKey != 'undefined');
+  for (let i = 0; i < event['Records'].length; i++) {
+    let record = event['Records'][i];
+    if (record.eventName !== 'INSERT' && record.eventName !== 'MODIFY') {
+      console.log('Event is not INSERT or MODIFY, ignore');
+      continue;
+    }
+    if (record.dynamodb.NewImage.isPublish.BOOL === false) {
+      console.log('Article is not published, ignore');
+      continue;
+    }
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+    const tableName = process.env.UserProfile_Table;
+    let dbParams = {
+      TableName: tableName,
+      FilterExpression: 'emailSubscription = :emailSubscription',
+      ExpressionAttributeValues: {
+        ':emailSubscription': true,
+      },
+    };
+    let scanResults = [];
+    let items;
+    //没有处理多于50个人的情况
+    do {
+      items = await docClient.scan(dbParams).promise();
+      items.Items.forEach((item) => scanResults.push(item));
+      dbParams.ExclusiveStartKey = items.LastEvaluatedKey;
+    } while (typeof items.LastEvaluatedKey != 'undefined');
 
-  const emailArray = scanResults
-    .map((item) => {
-      return item.email;
-    })
-    .filter((item) => item);
+    const emailArray = scanResults
+      .map((item) => {
+        return item.email;
+      })
+      .filter((item) => item);
 
-  console.log(emailArray);
+    console.log(emailArray);
 
-  let noDupEmailArray = [...new Set(emailArray)];
-  console.log(noDupEmailArray);
+    let noDupEmailArray = [...new Set(emailArray)];
+    console.log(noDupEmailArray);
 
-  const CHARSET = 'UTF-8';
-  const HTML_EMAIL_CONTENT = email_template.articlePublishTemplate(record);
-  const params = {
-    Destination: {
-      BccAddresses: noDupEmailArray,
-    },
-    Source: '"UWCSSA" <uwcssa-noreply@uwcssa.ca>',
-    Message: {
-      Body: {
-        Html: {
+    const CHARSET = 'UTF-8';
+    const HTML_EMAIL_CONTENT = email_template.articlePublishTemplate(record);
+    const params = {
+      Destination: {
+        BccAddresses: noDupEmailArray,
+      },
+      Source: '"UWCSSA" <uwcssa-noreply@uwcssa.ca>',
+      Message: {
+        Body: {
+          Html: {
+            Charset: CHARSET,
+            Data: HTML_EMAIL_CONTENT,
+          },
+        },
+        Subject: {
           Charset: CHARSET,
-          Data: HTML_EMAIL_CONTENT,
+          Data: `UWCSSA News: ${record.dynamodb.NewImage.title.S}`,
         },
       },
-      Subject: {
-        Charset: CHARSET,
-        Data: `UWCSSA News: ${record.dynamodb.NewImage.title.S}`,
-      },
-    },
-    ReplyToAddresses: ['uwincssa.it@gmail.com'],
-  };
+      ReplyToAddresses: ['uwincssa.it@gmail.com'],
+    };
 
-  const response = await ses.sendEmail(params).promise();
-  console.log(response);
+    const response = await ses.sendEmail(params).promise();
+    console.log(response);
+  }
 
   return Promise.resolve('Successfully processed DynamoDB record');
 };
--- amplify/backend/function/UserImageCompressHandler/src/index.js
+++ amplify/backend/function/UserImageCompressHandler/src/index.js
@@ -2,7 +2,7 @@
  * @Author: Shen Shu
  * @Date: 2022-05-24 23:30:45
  * @LastEditors: Shen Shu
- * @LastEditTime: 2022-06-01 23:01:52
+ * @LastEditTime: 2022-06-11 19:55:12
  * @FilePath: /uwcssa_ca/amplify/backend/function/UserImageCompressHandler/src/index.js
  * @Description:
  *
@@ -22,7 +22,8 @@ Amplify Params - DO NOT EDIT */
 const AWS = require('aws-sdk');
 const util = require('util');
 const sharp = require('sharp');
-
+const srcBucket = process.env.STORAGE_S3UWCSSATS_BUCKETNAME;
+const dstBucket = process.env.STORAGE_S3UWCSSATS_BUCKETNAME;
 // get reference to S3 client
 const s3 = new AWS.S3();
 
@@ -32,92 +33,89 @@ exports.handler = async (event, context, callback) => {
     'Reading options from event:\n',
     util.inspect(event, { depth: 5 }),
   );
-  const srcBucket = process.env.STORAGE_S3UWCSSATS_BUCKETNAME;
-  const dstBucket = process.env.STORAGE_S3UWCSSATS_BUCKETNAME;
+  for (let i = 0; i < event['Records'].length; i++) {
+    let record = event['Records'][i];
+    // Object key may have spaces or unicode non-ASCII characters.
+    const srcKey = decodeURIComponent(
+      record.dynamodb.NewImage.key.S.replace(/\+/g, ' '),
+    );
+    const typeMatch = srcKey.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+      console.log('Could not determine the image type.');
+      continue;
+    }
 
-  // Object key may have spaces or unicode non-ASCII characters.
-  const srcKey = decodeURIComponent(
-    event.Records[0].dynamodb.NewImage.key.S.replace(/\+/g, ' '),
-  );
-  const typeMatch = srcKey.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-    console.log('Could not determine the image type.');
-    return;
-  }
+    const imageType = typeMatch[1].toLowerCase();
+    if (!['jpeg', 'jpg', 'png', 'webp', 'svg'].includes(imageType)) {
+      console.log(`Unsupported image type: ${imageType}`);
+      continue;
+    }
 
-  const imageType = typeMatch[1].toLowerCase();
-  if (!['jpeg', 'jpg', 'png', 'webp', 'svg'].includes(imageType)) {
-    console.log(`Unsupported image type: ${imageType}`);
-    return;
-  }
+    const compressedDstKey = decodeURIComponent(
+      record.dynamodb.NewImage.objectCompressedURL.S.replace(/\+/g, ' '),
+    ).split('amazonaws.com/')[1];
 
-  const compressedDstKey = decodeURIComponent(
-    event.Records[0].dynamodb.NewImage.objectCompressedURL.S.replace(
-      /\+/g,
-      ' ',
-    ),
-  ).split('amazonaws.com/')[1];
+    const thumbnailDstKey = decodeURIComponent(
+      record.dynamodb.NewImage.objectThumbnailURL.S.replace(/\+/g, ' '),
+    ).split('amazonaws.com/')[1];
 
-  const thumbnailDstKey = decodeURIComponent(
-    event.Records[0].dynamodb.NewImage.objectThumbnailURL.S.replace(/\+/g, ' '),
-  ).split('amazonaws.com/')[1];
+    try {
+      const params = {
+        Bucket: srcBucket,
+        Key: srcKey,
+      };
+      var originImg = await s3.getObject(params).promise();
+    } catch (error) {
+      console.log(error);
+      return;
+    }
 
-  try {
-    const params = {
-      Bucket: srcBucket,
-      Key: srcKey,
+    const compressedWidth = record.dynamodb.NewImage.compressedWidth.N;
+    const thumbnailWidth = record.dynamodb.NewImage.thumbnailWidth.N;
+
+    let [compressedBuffer, thumbnailBuffer] = await Promise.all([
+      sharp(originImg.Body).resize(parseInt(compressedWidth)).toBuffer(),
+      sharp(originImg.Body).resize(parseInt(thumbnailWidth)).toBuffer(),
+    ]);
+
+    const compressedDstParams = {
+      Bucket: dstBucket,
+      Key: compressedDstKey,
+      Body: compressedBuffer,
+      ContentType: 'image',
     };
-    var originImg = await s3.getObject(params).promise();
-  } catch (error) {
-    console.log(error);
-    return;
+    const thumbnailDstPrams = {
+      Bucket: dstBucket,
+      Key: thumbnailDstKey,
+      Body: thumbnailBuffer,
+      ContentType: 'image',
+    };
+
+    let [compressedResult, thumbnailResult] = await Promise.all([
+      s3.putObject(compressedDstParams).promise(),
+      s3.putObject(thumbnailDstPrams).promise(),
+    ]);
+    console.log(
+      'Successfully resized ' +
+        srcBucket +
+        '/' +
+        srcKey +
+        ' and uploaded to ' +
+        dstBucket +
+        '/' +
+        compressedDstKey,
+      compressedResult,
+    );
+    console.log(
+      'Successfully resized ' +
+        srcBucket +
+        '/' +
+        srcKey +
+        ' and uploaded to ' +
+        dstBucket +
+        '/' +
+        thumbnailDstKey,
+      thumbnailResult,
+    );
   }
-
-  const compressedWidth = event.Records[0].dynamodb.NewImage.compressedWidth.N;
-  const thumbnailWidth = event.Records[0].dynamodb.NewImage.thumbnailWidth.N;
-
-  let [compressedBuffer, thumbnailBuffer] = await Promise.all([
-    sharp(originImg.Body).resize(parseInt(compressedWidth)).toBuffer(),
-    sharp(originImg.Body).resize(parseInt(thumbnailWidth)).toBuffer(),
-  ]);
-
-  const compressedDstParams = {
-    Bucket: dstBucket,
-    Key: compressedDstKey,
-    Body: compressedBuffer,
-    ContentType: 'image',
-  };
-  const thumbnailDstPrams = {
-    Bucket: dstBucket,
-    Key: thumbnailDstKey,
-    Body: thumbnailBuffer,
-    ContentType: 'image',
-  };
-
-  let [compressedResult, thumbnailResult] = await Promise.all([
-    s3.putObject(compressedDstParams).promise(),
-    s3.putObject(thumbnailDstPrams).promise(),
-  ]);
-  console.log(
-    'Successfully resized ' +
-      srcBucket +
-      '/' +
-      srcKey +
-      ' and uploaded to ' +
-      dstBucket +
-      '/' +
-      compressedDstKey,
-    compressedResult,
-  );
-  console.log(
-    'Successfully resized ' +
-      srcBucket +
-      '/' +
-      srcKey +
-      ' and uploaded to ' +
-      dstBucket +
-      '/' +
-      thumbnailDstKey,
-    thumbnailResult,
-  );
 };
