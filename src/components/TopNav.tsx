eda27e9ce8e4e78aa62bbaf8d310688cc2fe5a7f
Shikai <shikaijin7@gmail.com>
2022-06-21 01:47:02 -0400

    fix: update topnav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,15 +1,20 @@
 /*
  * @Author: 李佳修
  * @Date: 2022-05-18 09:12:03
- * @LastEditTime: 2022-05-18 14:37:02
- * @LastEditors: 李佳修
- * @FilePath: /uwcssa_ca/frontend/src/components/TopNav.tsx
+ * @LastEditTime: 2022-06-21 01:27:42
+ * @LastEditors: Shikai Jin
+ * @FilePath: /uwcssa_ca/src/components/TopNav.tsx
  */
+
+import { Button, IconButton } from '@mui/material';
+
 import Box from '@mui/material/Box';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from '@mui/material/Link';
 import React from 'react';
 import ThemeModeToggler from 'components/ThemeModeToggler';
 import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
 
 interface Props {
   colorInvert?: boolean;
@@ -19,15 +24,35 @@ const TopNav = ({ colorInvert = false }: Props): JSX.Element => {
   return (
     <Box display={'flex'} justifyContent={'flex-end'} alignItems={'center'}>
       <Box marginRight={{ xs: 1, sm: 2 }}>
+        <IconButton
+          size="small"
+          onClick={() =>
+            window.open('https://github.com/ShenShu2016/uwcssa_ca')
+          }
+          sx={{
+            color: colorInvert ? blue[50] : blue[700],
+            border: '1px solid #bdbdbd',
+            borderRadius: '10px',
+          }}
+        >
+          <GitHubIcon
+            fontSize="small"
+            sx={{
+              height: '20px',
+              width: '20px',
+            }}
+          />
+        </IconButton>
+      </Box>
+      {/* <Box marginRight={{ xs: 1, sm: 2 }}>
         <Link
           underline="none"
           component="a"
-          href="/demos"
+          href="/https://github.com/ShenShu2016/uwcssa_cademos"
           color={colorInvert ? 'common.white' : 'text.primary'}
           sx={{ display: 'flex', alignItems: 'center' }}
         >
-          Demos
-          <Box
+        <Box
             padding={0.5}
             display={'inline-flex'}
             borderRadius={1}
@@ -40,18 +65,18 @@ const TopNav = ({ colorInvert = false }: Props): JSX.Element => {
             >
               new
             </Typography>
-          </Box>
-        </Link>
+          </Box> 
+        </Link> 
       </Box>
       <Box marginRight={{ xs: 1, sm: 2 }}>
         <Link
           underline="none"
           component="a"
-          href="/blocks"
+          href="https://github.com/ShenShu2016/uwcssa_ca/issues"
           color={colorInvert ? 'common.white' : 'text.primary'}
           sx={{ display: 'flex', alignItems: 'center' }}
         >
-          Components
+          Issues
         </Link>
       </Box>
       <Box marginRight={{ xs: 1, sm: 2 }}>
@@ -63,7 +88,7 @@ const TopNav = ({ colorInvert = false }: Props): JSX.Element => {
         >
           Docs
         </Link>
-      </Box>
+      </Box> */}
       <Box>
         <ThemeModeToggler />
       </Box>
