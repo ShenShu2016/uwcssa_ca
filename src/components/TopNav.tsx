d5c56702b882dd9eee1b389701fef0054225f4bf
Shikai <shikaijin7@gmail.com>
2022-06-21 22:02:23 -0400

    feat: add react-github-btn

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: 李佳修
  * @Date: 2022-05-18 09:12:03
- * @LastEditTime: 2022-06-21 01:27:42
+ * @LastEditTime: 2022-06-21 21:59:44
  * @LastEditors: Shikai Jin
  * @FilePath: /uwcssa_ca/src/components/TopNav.tsx
  */
@@ -9,6 +9,8 @@
 import { Button, IconButton } from '@mui/material';
 
 import Box from '@mui/material/Box';
+import ChatIcon from '@mui/icons-material/Chat';
+import GitHubButton from 'react-github-btn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from '@mui/material/Link';
 import React from 'react';
@@ -23,6 +25,72 @@ interface Props {
 const TopNav = ({ colorInvert = false }: Props): JSX.Element => {
   return (
     <Box display={'flex'} justifyContent={'flex-end'} alignItems={'center'}>
+      <Box marginRight={{ xs: 1, sm: 2 }} marginTop={{ xs: 1 }}>
+        <GitHubButton
+          href="https://github.com/ShenShu2016/uwcssa_ca"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star ntkme/github-buttons on GitHub"
+        >
+          Star
+        </GitHubButton>
+
+        {/* <Link
+          underline="none"
+          component="a"
+          href="/https://github.com/ShenShu2016/uwcssa_cademos"
+          color={colorInvert ? 'common.white' : 'text.primary'}
+          sx={{ display: 'flex', alignItems: 'center' }}
+        >
+          <Box
+            padding={0.5}
+            display={'inline-flex'}
+            borderRadius={1}
+            bgcolor={'primary.main'}
+            marginLeft={1}
+          >
+            <Typography
+              variant={'caption'}
+              sx={{ color: 'common.white', lineHeight: 1 }}
+            >
+              new
+            </Typography>
+          </Box>
+        </Link> */}
+      </Box>
+      <Box marginRight={{ xs: 1, sm: 2 }} marginTop={{ xs: 1 }}>
+        <GitHubButton
+          href="https://github.com/ShenShu2016/uwcssa_ca/issues"
+          data-icon="octicon-issue-opened"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Issue ShenShu2016/uwcssa_ca on GitHub"
+        >
+          Issue
+        </GitHubButton>
+      </Box>
+      <Box marginRight={{ xs: 1, sm: 2 }}>
+        <IconButton
+          size="small"
+          onClick={() =>
+            window.open('https://github.com/ShenShu2016/uwcssa_ca/discussions')
+          }
+          sx={{
+            color: colorInvert ? blue[50] : blue[700],
+            border: '1px solid #bdbdbd',
+            borderRadius: '10px',
+          }}
+        >
+          <ChatIcon
+            fontSize="small"
+            sx={{
+              height: '20px',
+              width: '20px',
+            }}
+          />
+        </IconButton>
+      </Box>
       <Box marginRight={{ xs: 1, sm: 2 }}>
         <IconButton
           size="small"
@@ -44,51 +112,6 @@ const TopNav = ({ colorInvert = false }: Props): JSX.Element => {
           />
         </IconButton>
       </Box>
-      {/* <Box marginRight={{ xs: 1, sm: 2 }}>
-        <Link
-          underline="none"
-          component="a"
-          href="/https://github.com/ShenShu2016/uwcssa_cademos"
-          color={colorInvert ? 'common.white' : 'text.primary'}
-          sx={{ display: 'flex', alignItems: 'center' }}
-        >
-        <Box
-            padding={0.5}
-            display={'inline-flex'}
-            borderRadius={1}
-            bgcolor={'primary.main'}
-            marginLeft={1}
-          >
-            <Typography
-              variant={'caption'}
-              sx={{ color: 'common.white', lineHeight: 1 }}
-            >
-              new
-            </Typography>
-          </Box> 
-        </Link> 
-      </Box>
-      <Box marginRight={{ xs: 1, sm: 2 }}>
-        <Link
-          underline="none"
-          component="a"
-          href="https://github.com/ShenShu2016/uwcssa_ca/issues"
-          color={colorInvert ? 'common.white' : 'text.primary'}
-          sx={{ display: 'flex', alignItems: 'center' }}
-        >
-          Issues
-        </Link>
-      </Box>
-      <Box marginRight={{ xs: 1, sm: 2 }}>
-        <Link
-          underline="none"
-          component="a"
-          href="/docs/introduction"
-          color={colorInvert ? 'common.white' : 'text.primary'}
-        >
-          Docs
-        </Link>
-      </Box> */}
       <Box>
         <ThemeModeToggler />
       </Box>
